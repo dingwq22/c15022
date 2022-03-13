a0bf741644b250d05e0311314826329198b800ae
Xinyang Chen <51056554+Alan-Chen99@users.noreply.github.com>
2022-03-13 14:28:44 +0000

    more utils

--- utils.py
+++ utils.py
@@ -18,4 +18,23 @@ def loggingtimenow(*args):
 	return timestringnow().timetuple()
 
 def format_exception(error):
-	return ''.join(traceback.format_exception(type(error), error, error.__traceback__))
+	return "".join(traceback.TracebackException.from_exception(error).format())
+
+
+
+import base64
+from passlib import pwd
+from passlib.hash import sha512_crypt
+
+
+def tocode(s):
+	return base64.b16encode(s.encode()).decode()
+def fromcode(s):
+	return base64.b16decode(s.encode()).decode()
+
+def generate_password():
+	pwd.genword(entropy=512)
+def hash_password(password):
+	return tocode(sha512_crypt.hash(password,rounds=1000))
+def verify_password(password,hash):
+	return sha512_crypt.verify(password,fromcode(hash))
