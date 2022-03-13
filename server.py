3f9c28cae48e50f62551be6076a3b4f03a2c6f0f
Xinyang Chen <51056554+Alan-Chen99@users.noreply.github.com>
2022-03-13 16:10:26 +0000

    changes

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from aiohttp import web
-from aiohttp.web import Response
+from aiohttp.web import Response,json_response
+from utils import verify_password
+import database as db
+import os
 
 app = web.Application()
 routes = web.RouteTableDef()
@@ -9,9 +12,18 @@ async def homepage(req):
 	return Response(text="hello world")
 
 @routes.post("/db")
-async def database(req):
-	print(repr(req))
-	return Response(text="hello world")
+async def database_request(req):
+	data=await req.json()
+	if verify_password(data["password"], os.getenv("password_hash")):
+		if data["type"]=="get":
+			ans=await db.get(data["key"])
+		elif data["type"]=="set":
+			ans=await db.set(data["key"],data["val"])
+		elif data["type"]=="getall":
+			ans=await db.getall()
+		return json_response(ans)
+
+
 
 async def startserver():
 	app.add_routes(routes)
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 logging_channel=952348212151349269
-database="local"
+database="replit"
 replurl="https://c15022.repl.co/db"
 
 
