3d7e82b2d8708dd624a675b04601cd9d19c001a0
Alan Chen <chenxy@mit.edu>
2022-03-13 12:07:32 -0400

    database

--- database.py
+++ database.py
@@ -6,42 +6,43 @@ import setup
 import asyncio
 import logging
 
+from aiohttp import ClientResponseError
+
+sem = asyncio.Semaphore(3)
+errorsem=asyncio.Semaphore(1)
+
+async def tryrun(coro,*args,**kargs):
+	async with sem:
+		try:
+			return await coro(*args,**kargs)
+		except ClientResponseError:
+			pass
+		async with errorsem:
+			retrynum=0
+			waittime=1
+			while True:
+				retrynum=retrynum+1
+				try:
+					return await coro(*args,**kargs)
+				except ClientResponseError as e:
+					try:
+						raise dberror(f'failed to run {repr(coro)} with arguments {str(args)}, {str(kargs)} in try number {retrynum}') from e
+					except dberror as e2:
+						if retrynum<10:
+							logging.exception(e2)
+							await asyncio.sleep(waittime)
+							waittime*=2
+						else:
+							raise e2
+
+						
 if setup.database=="replit":
 	from replit.database import AsyncDatabase
 	from replit import db
-	from aiohttp import ClientResponseError
 	repldb=AsyncDatabase(db.db_url)
 	
 	class dberror(Exception):
 		pass
-	
-	sem = asyncio.Semaphore(3)
-	errorsem=asyncio.Semaphore(1)
-	
-	async def tryrun(coro,*args,**kargs):
-		async with sem:
-			try:
-				return await coro(*args,**kargs)
-			except ClientResponseError:
-				pass
-			async with errorsem:
-				retrynum=0
-				waittime=1
-				while True:
-					retrynum=retrynum+1
-					try:
-						return await coro(*args,**kargs)
-					except ClientResponseError as e:
-						try:
-							raise dberror(f'failed to run {repr(coro)} with arguments {str(args)}, {str(kargs)} in try number {retrynum}') from e
-						except dberror as e2:
-							if retrynum<10:
-								logging.exception(e2)
-								await asyncio.sleep(waittime)
-								waittime*=2
-							else:
-								raise e2
-	
 	
 	async def tryset(key:str,val):
 		if val==None:
@@ -73,3 +74,32 @@ if setup.database=="replit":
 		for x in await tryrun(repldb.keys):
 			ans[x]=await get(x)
 		return ans
+
+elif setup.database=="local":
+	print(setup.replurl)
+	import os
+	import aiohttp
+	session=aiohttp.ClientSession()
+	async def tryget(key:str):
+		async with session.post(setup.replurl, json={"password":os.getenv("password"), "type": "get", "key": key}) as resp:
+			return await resp.json()
+	async def get(key:str):
+		return tryrun(tryget, key)
+
+	async def tryset(key:str, val):
+		async with session.post(setup.replurl, json={"password":os.getenv("password"), "type": "set", "key": key, "val": val}) as resp:
+			return await resp.json()
+	async def set(key:str, val):
+		return tryrun(tryset, key, val)
+
+	async def trygetall():
+		async with session.post(setup.replurl, json={"password":os.getenv("password"), "type": "getall"}) as resp:
+			return await resp.json()
+	async def getall():
+		return await tryrun(trygetall)
+
+
+
+
+
+	
--- setup.py
+++ setup.py
@@ -1,2 +1,5 @@
 logging_channel=952348212151349269
-database="replit"
+database="local"
+replurl="https://c15022.repl.co/db"
+
+
